adc168bbf65a4f1dfb6d47d9e42aaf2a12e526be
RDXV <varunarora1408@gmail.com>
2022-08-10 00:09:12 +0530

    single person chat with List<String> messages

---
+++ lib/chat.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+// define comtrollers in state and not above
+class _ChatState extends State<Chat> {
+  late TextEditingController _controller;
+  List<String> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat App'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            MessageView(messages: messages),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Type a message',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        messages.add(_controller.text);
+                        debugPrint(_controller.text);
+                        _controller.clear();
+                      });
+                    },
+                    child: const Text('Send')),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageView extends StatefulWidget {
+  MessageView({Key? key, required this.messages}) : super(key: key);
+
+  late List<String> messages = [];
+  @override
+  State<MessageView> createState() => _MessageViewState();
+}
+
+class _MessageViewState extends State<MessageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            children: widget.messages
+                .map((e) => Container(
+                    margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 98, 209, 198),
+                        border: Border.all(width: 2, color: Colors.white),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10))),
+                    child: Text(e)))
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
